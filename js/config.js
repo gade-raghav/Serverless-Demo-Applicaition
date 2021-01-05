c1b852ab35d52f6fc6ce87c5518f536182661733
gade-raghav <gaderaghav5@gmail.com>
2021-01-05 21:24:02 +0530

    assignmet repo

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-2_gzDV9gx7a', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: 'm9pmg4uo7h05pc9qdbqrr7v98', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-2' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
